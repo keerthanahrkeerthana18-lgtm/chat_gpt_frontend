479520e0ea41fd83dfd7f552525bf633f1f006a5
keerthanahrkeerthana18-lgtm <keerthanahrkeerthana18@gmail.com>
2026-02-11 12:49:04 +0530

    login

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -5,16 +5,21 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 function App() {
   return (
     <Router>
-     <Header/>
-    
+      <Header/>
+      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer/>
     </Router>
--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
-    return (
-        <div>Header
-            
-       <nav style={{ padding: '1rem', backgroundColor: '#333', marginBottom: '1rem' }}>
+  return (
+    <div>
+        <nav style={{ padding: '1rem', backgroundColor: '#333', marginBottom: '1rem' }}>
         <Link to="/" style={{ color: 'white', marginRight: '1rem' }}>Home</Link>
         <Link to="/about" style={{ color: 'white', marginRight: '1rem' }}>About</Link>
-        <Link to="/contact" style={{ color: 'white' }}>Contact</Link>
+        <Link to="/contact" style={{ color: 'white', marginRight: '1rem' }}>Contact</Link>
+        <Link to="/login" style={{ color: 'white', marginRight: '1rem' }}>Login</Link>
+        <Link to="/signup" style={{ color: 'white', marginRight: '1rem' }}>Signup</Link>
       </nav>
-      </div>
-    )
+    </div>
+  )
 }
 
 export default Header
+
+// login sign up needts to be added , logo needs to be added
